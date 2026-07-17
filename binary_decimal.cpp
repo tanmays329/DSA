7fafd21a843ad6cc458f32c5c47bb181eac5ea62
tanmays329 <tanmays524@gmail.com>
2026-07-17 11:25:10 +0530

    Add binary_decimal.cpp

---
+++ binary_decimal.cpp
@@ -0,0 +1,44 @@
+// convert decimal into binary
+
+// #include <iostream>
+// #include<math.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin>>n;
+
+//     int ans=0;
+//     int i=0;
+//     while(n!=0) {
+//         int bit = n & 1;
+//         ans = (bit * pow(10,i)) +ans;
+//         n = n >> 1;
+//         i++;
+//     }
+//     cout<<ans;
+// }
+
+
+// binary to decimal conversion
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >>n;
+
+    int i=0;
+    int ans=0;
+    while(n!=0) {
+        int digit=n%10;
+        if(digit==1) {
+            ans=ans+pow(2,i);
+        }
+        n=n/10;
+        i++;
+    }
+    cout<<ans;
+}
