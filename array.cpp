ab3e91d80a0532f3db4960f4f41358d2f6c44582
tanmays329 <tanmays524@gmail.com>
2026-07-25 02:26:58 +0530

    Add updated DSA practice files

---
+++ array.cpp
@@ -0,0 +1,66 @@
+// #include<iostream>
+// using namespace std;
+
+// void printArray(int arr[], int size) {
+//     for (int i = 0; i < size; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// int main() {
+
+
+//     int number[15];
+//     printArray(number, 15);
+
+
+//     int second[3] = {10, 20, 30};
+//     printArray(second, 3);
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+// int getMax(int num[], int n) {
+    
+//     int maxi = INT_MIN;
+//     for (int i = 0; i < n; i++) {
+//         maxi = max(maxi, num[i]);
+//     }
+//     return maxi;
+
+// }
+
+// int getMin(int num[], int n) {
+    
+//     int mini = INT_MAX;
+//     for (int i = 0; i < n; i++) {
+//         mini = min(mini, num[i]);
+//     }
+//     return mini;
+
+// }
+
+
+// int main() {
+
+//     int size;
+//     cin >> size;
+
+//     int num[100];
+//     for (int i = 0; i < size; i++) {
+//         cin >> num[i];
+//     }
+
+
+//     cout << "Maximum element: " << getMax(num, size) << endl;
+//     cout << "Minimum element: " << getMin(num, size) << endl;
+
+//     return 0;
+// }
+
